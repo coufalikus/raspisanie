913bfcc8d95a1e0da356c4ac02b47e029be7f315
coufalikus <42027316+coufalikus@users.noreply.github.com>
2018-08-05 00:47:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
     bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте.")
 
-updater = Updater(token='TOKEN')  # тут токен, который выдал вам Ботский Отец!
+updater = Updater(token='646056076:AAHTUqGeOBavOu_FGnR8LmAK_zZ69D2vyq0')  # тут токен, который выдал вам Ботский Отец!
 
 start_handler = CommandHandler('start', start)  # этот обработчик реагирует
                                                 # только на команду /start
