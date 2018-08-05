ff989bcfeb9a445d0025e3d2446bf525fa623de8
coufalikus <42027316+coufalikus@users.noreply.github.com>
2018-08-05 15:03:53 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
     bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте.")
 def users(bot,update):
-	bot.sendmessage(chat_id=update.message.chat_id, str(telegram.getChatMembersCount(chat_id=update.message.chat_id)))
+	text=str(telegram.getChatMembersCount(chat_id=update.message.chat_id))
+	bot.sendmessage(chat_id=update.message.chat_id, text)
 
 updater = Updater(token='646056076:AAHTUqGeOBavOu_FGnR8LmAK_zZ69D2vyq0')  # тут токен, который выдал вам Ботский Отец!
 
