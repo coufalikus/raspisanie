2bba3c77832c085a8fbdcd4ce16630a4ed89b65c
coufalikus <42027316+coufalikus@users.noreply.github.com>
2018-08-05 15:06:41 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,10 +4,7 @@ from telegram.ext import CommandHandler  # модуль почему-то про
 
 def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
-    bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте.")
-def users(bot,update):
-	text=str(telegram.getChatMembersCount(chat_id=update.message.chat_id))
-	bot.sendmessage(chat_id=update.message.chat_id, text)
+    bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте. "+str(telegram.getChatMembersCount(chat_id=update.message.chat_id)))
 
 updater = Updater(token='646056076:AAHTUqGeOBavOu_FGnR8LmAK_zZ69D2vyq0')  # тут токен, который выдал вам Ботский Отец!
 
