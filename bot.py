77b42c295cc7d53f9ca058ad972670578223c3de
coufalikus <42027316+coufalikus@users.noreply.github.com>
2018-08-05 15:33:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,7 @@ from telegram.ext import CommandHandler  # модуль почему-то про
 
 def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
-    text="Здравствуйте. "
-    text+=str(telegram.getChatMembersCount(chat_id=update.message.chat_id))
-    bot.sendMessage(chat_id=update.message.chat_id, text=text)
+    bot.sendMessage(chat_id=update.message.chat_id, text=str(telegram.getChatMembersCount(chat_id=update.message.chat_id)))
                     
 updater = Updater(token='646056076:AAHTUqGeOBavOu_FGnR8LmAK_zZ69D2vyq0')  # тут токен, который выдал вам Ботский Отец!
 
