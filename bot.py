455b9969e633a72d11ac369060f317c69a0b60a0
coufalikus <42027316+coufalikus@users.noreply.github.com>
2018-08-05 17:07:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@
 #from telegram.ext import CommandHandler  # модуль почему-то просто telegram ¯\_(ツ)_/¯
 #from telegram.ext import MessageHandler
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+def add(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
     bot.sendMessage(chat_id=update.message.chat_id, text="Привет! ")
@@ -21,8 +22,10 @@ updater = Updater(token='646056076:AAHTUqGeOBavOu_FGnR8LmAK_zZ69D2vyq0')  # ту
 
 start_handler = CommandHandler('start', start)  # этот обработчик реагирует
                                                 # только на команду /start
+add_member = CommandHandler('add', addMember)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 
 updater.dispatcher.add_handler(start_handler)   # регистрируем в госреестре обработчиков
 updater.dispatcher.add_handler(text_message_handler)
+updater.dispatcher.add_handler(add_member)
 updater.start_polling()  # поехали!
